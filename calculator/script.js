55c49fb9a32131ed2d225781dbbcd548007758de
Aliaksandr Turbin <turbin28@gmail.com>
2020-09-23 02:12:35 +0300

    fix: script sum error

--- calculator/script.js
+++ calculator/script.js
@@ -3,7 +3,9 @@
 let buffer1 = "";// show on display
 let buffer2 = "";// memory
 let buttonFunction;
-let afterRez;
+let afterRez = 0;
+let lastNumber = 0;
+let clickIndex = false;
 
 const showDigital = (input = "0") => {
     document.getElementById("maindisplay").innerHTML = input;
@@ -12,6 +14,8 @@ const showDigital = (input = "0") => {
 const calculation = (num) => {
     buffer1 = buffer1 + num;
     showDigital(buffer1);
+    lastNumber = buffer1;
+    afterRez = lastNumber;
 }
 
 const reset = () => {
@@ -22,48 +26,37 @@ const reset = () => {
 }
 
 const result = (bf) => {
-
-
+    // buffer2 = buffer1;
 
     switch (bf) {
         case "sum":
-            console.log("+");
+            // console.log("+");
             afterRez = (buffer2 * 1) + (1 * buffer1);
             break;
-
         case "sub":
             console.log("-");
             afterRez = (buffer2 * 1) - (1 * buffer1);
             break;
-
         case "mul":
             console.log("*");
             afterRez = (buffer2 * 1) * (1 * buffer1);
             break;
-
         case "sul":
             console.log("/");
             afterRez = (buffer2 * 1) / (1 * buffer1);
             break;
-
         default:
+            // afterRez = "0"
             break;
     }
+    console.log(afterRez + "*")
+    // if (afterRez === 0) {
+    //     afterRez = lastNumber;
+    // }
 
-    // buffer2 = "";
-    showDigital(( Math.floor(afterRez * 100) / 100 ));
-    // afterRez = "";
-    // buffer1 = 
-    buffer1 = afterRez;
-    buffer2 = "";
-    afterRez = "";
 }
-const bufferResort = () => {
-    if (buffer2 === "") {
-        buffer2 = buffer1;
-        buffer1 = "";
-    }
-}
+
+
 
 dig1.onclick = function (event) { calculation(this.id[3]) }
 dig2.onclick = function (event) { calculation(this.id[3]) }
@@ -92,10 +85,22 @@ ac.onclick = function (event) {
 }
 
 plus.onclick = function (event) {
+    console.log("+")
     buttonFunction = "sum"
-    bufferResort();
-    showDigital(0);
+    // buffer1 = afterRez;
+    // buffer2 = "";
+    // afterRez = "";
+    buffer2 = afterRez;
+    buffer1 = "";
+    clickIndex = false;
+    showDigital((Math.floor(afterRez * 100) / 100) + "p");
+
 }
+// plus.onclick = function (event) {
+//     buttonFunction = "sum"
+//     bufferResort();
+//     showDigital(0);
+// }
 
 
 
@@ -119,7 +124,22 @@ sublime.onclick = function (event) {
 }
 
 rez.onclick = function (event) {
-    result(buttonFunction);
+    console.log("= " + buttonFunction + "+++" + lastNumber)
+    showDigital((Math.floor(afterRez * 100) / 100) + "r1");
+    buffer1 = afterRez;
+    buffer2 = "";
+    afterRez = "";
+    if (clickIndex === true) {
+        console.log("Yes!")
+        buffer2 = lastNumber;
+        result(buttonFunction);
+        showDigital((Math.floor(afterRez * 100) / 100) + "r2");
+        buffer1 = afterRez;
+        buffer2 = "";
+        afterRez = "";
+    }
+    clickIndex = true;
+
     // reset();
 
 }
@@ -128,7 +148,10 @@ rez.onclick = function (event) {
 // ! console help function 
 
 AllKeys.onclick = function (event) {
-    console.log("b1:" + buffer1 + " b2:" + buffer2 + " rez:" + afterRez + " f:" + buttonFunction);
+    console.log(`${buffer1}-${buffer2}-L${lastNumber}-R${afterRez}`)
+    result(buttonFunction);
+
+    // console.log("b1:" + buffer1 + " b2:" + buffer2 + " rez:" + afterRez + " f:" + buttonFunction);
 }
 
 
