4d310730f52988387cc447a2a5d80ac41988e9db
Aliaksandr Turbin <turbin28@gmail.com>
2020-10-22 13:20:06 +0300

    init: start momentum-task

---
+++ momentum/script.js
@@ -0,0 +1,6 @@
+console.log("dd")
+
+const time = document.querySelector('.time'),
+  greeting = document.querySelector('.greeting'),
+  name = document.querySelector('.name'),
+  focus = document.querySelector('.focus');
