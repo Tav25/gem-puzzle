33005fcaf23dc6d2be9c951831432dd29f7773a5
Aliaksandr Turbin <turbin28@gmail.com>
2020-10-29 22:47:54 +0300

    feat: add to text area input keycode

--- Virtual Keyboard/script.js
+++ Virtual Keyboard/script.js
@@ -5,6 +5,7 @@ let keyCaharCodeEn = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 1
 let keyCaharCodeEnUp = [126, 33, 64, 35, 36, 37, 94, 38, 42, 40, 41, 95, 43, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 123, 125, 124, 65, 83, 68, 70, 71, 72, 74, 75, 76, 58, 34, 90, 88, 67, 86, 66, 78, 77, 60, 62, 63, 32];
 let keyCaharCodeRu = [1105, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 1081, 1094, 1091, 1082, 1077, 1085, 1075, 1096, 1097, 1079, 1093, 1098, 92, 1092, 1099, 1074, 1072, 1087, 1088, 1086, 1083, 1076, 1078, 1101, 1103, 1095, 1089, 1084, 1080, 1090, 1100, 1073, 1102, 46, 32]
 let keyCaharCodeRuUp = [1025, 33, 34, 8470, 59, 37, 58, 63, 42, 40, 41, 95, 43, 1049, 1062, 1059, 1050, 1045, 1053, 1043, 1064, 1065, 1047, 1061, 1066, 47, 1060, 1067, 1042, 1040, 1055, 1056, 1054, 1051, 1044, 1046, 1069, 1071, 1063, 1057, 1052, 1048, 1058, 1068, 1041, 1070, 44, 32]
+
 let keyCodeArray = ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "Space"]
 
 let textArea = []
@@ -12,7 +13,7 @@ let textArea = []
 let lang = "ru"
 let shift = false
 
-let text = "hello world"
+let outText = "hello world"
 
 
 function getKeyToArray(array) {
@@ -23,7 +24,8 @@ function getKeyToArray(array) {
 }
 
 function playSoundKey() {
-    if (lang === "ru") { audioPath = "/Virtual Keyboard/assets/sound/mechanical-Keyboard-single-buttonRu.mp3" }
+    if (lang === "ru") { 
+        audioPath = "/Virtual Keyboard/assets/sound/mechanical-Keyboard-single-buttonRu.mp3" }
     else {
         audioPath = "/Virtual Keyboard/assets/sound/mechanical-Keyboard-single-buttonEn.mp3"
     }
@@ -63,6 +65,7 @@ function keyFlashSound(keyCode) {
 
     document.onkeydown = function (event) {
         document.querySelector("body > div.main > div.main-keyboard > div.button[data = '" + event.code + "']").classList.add("flash")
+        playSoundKey()
         keyCode = event.code
         cl(keyCode)
     }
@@ -111,8 +114,11 @@ function setShift() {
 
 }
 
+
 function makeText(text) {
-    document.querySelector("body > div.main > div.main-inputText").innerHTML = text
+
+    outText+=text
+    document.querySelector("body > div.main > div.main-inputText").innerHTML = outText
 }
 
 // getKeyToArray(keyCodeArray)
@@ -125,4 +131,4 @@ function makeText(text) {
 // keyFlashSound()
 
 makeKey(keyCaharCodeRu)
-makeText()
+// makeText()
