8a702879e420ee3b16ac4b082ef5986dcf7dfce5
alex <turbin28@gmail.com>
2020-11-10 01:57:30 +0300

    feat: add module game

--- gem-puzzle/src/Game.js
+++ gem-puzzle/src/Game.js
@@ -1,5 +1,5 @@
-export { g, gameX }
-let g = 10
+export { g, gameX, test };
+let g = 10;
 
 function cl(x) { console.log(x); }
 
@@ -7,9 +7,9 @@ const gameX = {
   move: 0,
   col: 4,
 
+  get gamePole() { return [1, 2, 3, 4, 5, 6, 0, 7, 8, 9, 10, 11, 12, 13, 14, 15]; },
 
-
-  get gamePole(){ return  [1, 2, 3, 4,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]},
+  get zeroPosition() { return this.gamePole.indexOf(0); },
 
   movePoint() {
     document.querySelector('#movesJS').innerHTML = this.move;
@@ -31,26 +31,42 @@ const gameX = {
   },
 
   initGameCell() {
-    console.log('initGameCell()')
-    let g = `<div class="wrapper-gamebox-cell"></div>`
-    let f = ''
+    console.log('initGameCell()');
+    const g = '<div class="wrapper-gamebox-cell"></div>';
+    let f = '';
     for (let i = 0; i < this.col * this.col; i++) {
-      console.log('dsdksjdks4')// ... тело цикла ...
-      f = f + `<div id= "cellId${i}" class="wrapper-gamebox-cell"></div>`
+      f += `<div id= "cellId${i}" class="wrapper-gamebox-cell"></div>`;
     }
-    document.querySelector("body > div.wrapper > div.wrapper-gamebox").innerHTML = f
-
+    document.querySelector('body > div.wrapper > div.wrapper-gamebox').innerHTML = f;
   },
 
   initNumbers() {
     console.log('cl');
     for (const i in this.gamePole) {
-      cl(this.gamePole[i]);
+      // cl(this.gamePole[i]);
       if (this.gamePole[i] !== 0) { document.querySelector(`#cellId${i}`).innerHTML = `<div id = 'number${this.gamePole[i]}' class="wrapper-gamebox-cell-number"><span>${this.gamePole[i]}</span></div>`; } else {
         cl(`добавляем свойства к ячейкам ${i}`);
       }
     }
   },
 
+  initZeroPoint() {
+    const zeroPositionCell = (this.zeroPosition + 1);
+    cl(`cell:${zeroPositionCell}`);
+    if (zeroPositionCell - 4 > 0) { cl(`y0 ${zeroPositionCell - 4}`); }
+    if (zeroPositionCell % 4 != 0) { cl(`y1 ${zeroPositionCell + 1}`); }
+    if (zeroPositionCell + 4 < 16) { cl(`y2 ${zeroPositionCell + 4}`); }
+    if (zeroPositionCell % 4 != 1) { cl(`y3 ${zeroPositionCell - 1}`); }
+  },
+
 };
 
+class NumberBut {
+  moveUp(){cl('up')}
+  moveDown(){cl('Down')}
+  moveLeft(){cl('Left')}
+  moveRight(){cl('Right')}
+}
+
+
+let test = new NumberBut
--- gem-puzzle/src/index.js
+++ gem-puzzle/src/index.js
@@ -1,8 +1,12 @@
 import css from './style.css';
 import * as game from './Game';
 
-console.log('index8' + game.g);
+console.log(`index8${game.g}`);
 
-game.gameX.initBox()
-game.gameX.initGameCell()
+game.gameX.initBox();
+game.gameX.initGameCell();
 game.gameX.initNumbers();
+game.gameX.initZeroPoint();
+game.test.moveDown();
+
+console.log(game)
