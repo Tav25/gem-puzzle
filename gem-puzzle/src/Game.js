cef367906deb61a3d36d383f2d88b1e08898e9d8
Aliaksandr Turbin <turbin28@gmail.com>
2020-11-11 20:08:31 +0300

    feat: add zero move func

--- gem-puzzle/src/Game.js
+++ gem-puzzle/src/Game.js
@@ -6,23 +6,19 @@ function cl(x) { console.log(x); }
 const gameX = {
   move: 0,
   col: 4,
-  gamePole: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 0,14, 15],
+  gamePole: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0],
+  movedNumber: [],
 
   // get gamePole() { return [1, 2, 3, 4, 5, 6, 0, 7, 8, 9, 10, 11, 12, 13, 14, 15]; },
 
   get zeroPosition() { return this.gamePole.indexOf(0); },
 
-  gamePoleZeroMove(y=0) {
-    const arr_ = this.gamePole;
-    const oldPlace = this.gamePole.indexOf(y)
-    const newPlace = this.gamePole.indexOf(0);
-
-    const t = arr_.splice(oldPlace, 1);
-    // после того как удалите элемент из массива, у него изменится длина
-    arr_.splice(newPlace, 0, t[0]);
-    console.log(arr_);
-
-    return arr_;
+  gamePoleZeroMove(a) {
+    const arr = this.gamePole;
+    const x = arr.indexOf(a);
+    const y = arr.indexOf(0);
+    arr[x] = arr.splice(y, 1, arr[x])[0];
+    console.log(arr);
   },
 
   movePoint() {
@@ -35,8 +31,8 @@ const gameX = {
         <div class="wrapper">
         <div class="wrapper-text">
             <div class="wrapper-text-time"> <span>00:00</span> </div>
-            <div class="wrapper-text-menu">menu</div>
-            <div class="wrapper-text-move">moves: <span id="movesJS">0</span></div>
+            <div class="wrapper-text-menu"> <span>1</span> <span>2</span> <span>3</span> </div>
+            <div class="wrapper-text-move">moves: <span id="movesJS">0</span></div> 
         </div>
         <div class="wrapper-gamebox">            
         </div>        
@@ -58,19 +54,55 @@ const gameX = {
     console.log('cl');
     for (const i in this.gamePole) {
       // cl(this.gamePole[i]);
-      if (this.gamePole[i] !== 0) { document.querySelector(`#cellId${i}`).innerHTML = `<div id = 'number${this.gamePole[i]}' class="wrapper-gamebox-cell-number"><span>${this.gamePole[i]}</span></div>`; } else {
+      if (this.gamePole[i] !== 0) { document.querySelector(`#cellId${i}`).innerHTML = `<div id = 'number${this.gamePole[i]}' class="wrapper-gamebox-cell-number" " ><span>${this.gamePole[i]}</span></div>`; } else {
         cl(`добавляем свойства к ячейкам ${i}`);
       }
     }
   },
 
   initZeroPoint() {
-    const zeroPositionCell = (this.zeroPosition + 1);
+    const zeroPositionCell = (this.gamePole.indexOf(0));
     cl(`cell:${zeroPositionCell}`);
-    if (zeroPositionCell - 4 > 0) { cl(`y0 ${zeroPositionCell - 4}`); }
-    if (zeroPositionCell % 4 != 0) { cl(`y1 ${zeroPositionCell + 1}`); }
-    if (zeroPositionCell + 4 < 16) { cl(`y2 ${zeroPositionCell + 4}`); }
-    if (zeroPositionCell % 4 != 1) { cl(`y3 ${zeroPositionCell - 1}`); }
+    if (zeroPositionCell - 4 >= 0) {
+      cl(`y0 ${zeroPositionCell - 4}`);
+      document.querySelector(`#cellId${zeroPositionCell - 4} > .wrapper-gamebox-cell-number`).classList.add('mystyle');
+      document.querySelector(`#cellId${zeroPositionCell - 4} > .wrapper-gamebox-cell-number`).addEventListener('mouseup', (e) => {
+        this.gamePoleZeroMove(this.gamePole[zeroPositionCell - 4]);
+        this.initGameCell();
+        this.initNumbers();
+        this.initZeroPoint();
+      });
+    }
+    if ((zeroPositionCell + 1) % 4 !== 0) {
+      cl(`y1 ${zeroPositionCell + 0}`);
+      document.querySelector(`#cellId${zeroPositionCell + 1} > .wrapper-gamebox-cell-number`).classList.add('mystyle');
+      document.querySelector(`#cellId${zeroPositionCell + 1} > .wrapper-gamebox-cell-number`).addEventListener('mouseup', (e) => {
+        this.gamePoleZeroMove(this.gamePole[zeroPositionCell + 1]);
+        this.initGameCell();
+        this.initNumbers();
+        this.initZeroPoint();
+      });
+    }
+    if (zeroPositionCell + 4 < 16) {
+      cl(`y2 ${zeroPositionCell + 4}`);
+      document.querySelector(`#cellId${zeroPositionCell + 4} > .wrapper-gamebox-cell-number`).classList.add('mystyle');
+      document.querySelector(`#cellId${zeroPositionCell + 4} > .wrapper-gamebox-cell-number`).addEventListener('mouseup', (e) => {
+        this.gamePoleZeroMove(this.gamePole[zeroPositionCell + 4]);
+        this.initGameCell();
+        this.initNumbers();
+        this.initZeroPoint();
+      });
+    }
+    if ((zeroPositionCell + 1) % 4 !== 1) {
+      cl(`P y3 ${zeroPositionCell - 1}`);
+      document.querySelector(`#cellId${zeroPositionCell - 1} > .wrapper-gamebox-cell-number`).classList.add('mystyle');
+      document.querySelector(`#cellId${zeroPositionCell - 1} > .wrapper-gamebox-cell-number`).addEventListener('mouseup', (e) => {
+        this.gamePoleZeroMove(this.gamePole[zeroPositionCell - 1]);
+        this.initGameCell();
+        this.initNumbers();
+        this.initZeroPoint();
+      });
+    }
   },
 
 };
@@ -86,3 +118,6 @@ class NumberBut {
 }
 
 let test = new NumberBut();
+
+// document.querySelector(`#cellId${zeroPositionCell - 1}`).classList.add('animated');
+// document.querySelector(`#cellId${zeroPositionCell - 1} > .wrapper-gamebox-cell-number`).style.marginTop = '100px';
--- gem-puzzle/src/index.js
+++ gem-puzzle/src/index.js
@@ -8,13 +8,42 @@ game.gameX.initBox();
 game.gameX.initGameCell();
 game.gameX.initNumbers();
 game.gameX.initZeroPoint();
-game.gameX.gamePoleZeroMove(13);
-game.gameX.initGameCell();
-game.gameX.initNumbers();
-game.gameX.gamePoleZeroMove(10);
-game.gameX.initGameCell();
-game.gameX.initNumbers();
 
-game.test.moveDown();
-console.log(game.gameX.zeroPosition);
-console.log(game);
+function ad(a) {
+  game.gameX.gamePoleZeroMove(a);
+  game.gameX.initGameCell();
+  game.gameX.initNumbers();
+  game.gameX.initZeroPoint();
+}
+
+const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+async function show() {
+  await sleepNow(1000);
+  ad(12, 0);
+  await sleepNow(1000);
+  ad(11, 0);
+  await sleepNow(1000);
+  ad(10, 0);
+  await sleepNow(1000);
+  ad(9, 0);
+  await sleepNow(1000);
+  ad(5, 0);
+  await sleepNow(1000);
+  ad(6, 0);
+//   await sleepNow(1000);
+//   ad(2, 0);
+//   await sleepNow(1000);
+//   ad(1, 0);
+}
+
+show();
+
+// async function repeatedGreetingsLoop() {
+//   for (let i = 1; i <= 5; i++) {
+//     await sleepNow(1000)
+//     console.log(`Hello #${i}`)
+//   }
+// }
+
+// repeatedGreetingsLoop()
